a751082ab0cad405a21aa2138e32196fa7c7323e
TechnoWolf96 <v.alentin52.v@yandex.ru>
2022-01-05 21:26:41 +0300

    Добавил на сцену динамический свет

--- Assets/Scripts/LightFlicker.cs
+++ Assets/Scripts/LightFlicker.cs
@@ -1,30 +1,34 @@
+using FunkyCode;
 using UnityEngine;
 
 public class LightFlicker : MonoBehaviour
 {
-    public float maxIntensity;
+    //public float maxIntensity;
     public float minIntensity;
+    public float startIntensity = 0;
     public float speedChanging;
+    public bool startWithRandomIntensity;
 
-    private Light light;
+    private Light2D light;
     private bool increase = true;
 
     private void Start()
     {
-        light = GetComponent<Light>();
-        light.intensity = minIntensity;
+        light = GetComponent<Light2D>();
+        if (startWithRandomIntensity) light.color.a = Random.Range(minIntensity, 1f);
+        else light.color.a = startIntensity;
     }
     private void FixedUpdate()
     {
         if (increase)
         {
-            light.intensity += Time.deltaTime * speedChanging;
-            if (light.intensity > maxIntensity) increase = false;
+            light.color.a += Time.deltaTime * speedChanging;
+            if (light.color.a >= 1) increase = false;
         }
         else
         {
-            light.intensity -= Time.deltaTime * speedChanging;
-            if (light.intensity < minIntensity) increase = true;
+            light.color.a -= Time.deltaTime * speedChanging;
+            if (light.color.a < minIntensity) increase = true;
         }
     }
 
