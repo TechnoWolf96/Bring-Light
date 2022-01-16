c0fd822afe733b8d3b7f24b47c9867a30dd4c432
TechnoWolf96 <v.alentin52.v@yandex.ru>
2022-01-16 11:19:50 +0300

    Добавил герою пляшущие огоньки, которые нужно еще доделать

--- Assets/Scripts/Creatures/Mobs/AssistantLight.cs
+++ Assets/Scripts/Creatures/Mobs/AssistantLight.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class AssistantLight : SmartDistance
 {
     [Header("Assistant Light:")]
-    public Transform positionLight;
+    public Creature positionLight;
 
     protected override void Start()
     {
         base.Start();
-        //follow = positionLight;
+        follow = positionLight;
     }
 }
